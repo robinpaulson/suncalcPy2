cf5c01b9c345998ad62dd8da697aab6ef00813bb
Robin Paulson <robin@bumblepuppy.org>
2021-10-29 16:36:46 +1300

    tidy

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from setuptools import setup
 setup(
     name='suncalcPy2',
     version='0.1.0',
-    description='A lightweight',
+    description='A lightweight module for calculating the location of celestial objects',
     url='https://github.com/robinpaulson/suncalcPy2',
     author='Robin Paulson',
     author_email='robin@bumblepuppy.org',
     license='MIT',
-    packages=['pyexample'],
 
     classifiers=[
         'Development Status :: 1 - Planning',
@@ -19,6 +18,9 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
 )
