1d5d498b2d103bf79992c591101e01cab7eab66a
Robin Paulson <robin@bumblepuppy.org>
2021-10-29 13:34:41 +1300

    added pip setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='suncalcPy2',
+    version='0.1.0',
+    description='A lightweight',
+    url='https://github.com/robinpaulson/suncalcPy2',
+    author='Robin Paulson',
+    author_email='robin@bumblepuppy.org',
+    license='MIT',
+    packages=['pyexample'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
